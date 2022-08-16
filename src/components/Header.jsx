04d1ed868b6400e2b0e8e6c3a052a730dbe1f9e4
Camila Reis <79461028+cxavier6@users.noreply.github.com>
2022-08-16 17:22:31 -0300

    adiciona arquivos base e header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+    return ( 
+    <div className="container-header">
+        <div className="header">
+            <p className="header-title">uma seleção de produtos</p>
+            <h2>especial para você</h2>
+            <p className="header-info">Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+            <div className="header-button">
+                <button>Conheça a Linux</button>
+                <button>Ajude o algorítimo</button>
+                <button>Seus produtos</button>
+                <button>Compartilhe</button>
+            </div>
+        </div>
+    </div>
+    )
+}
